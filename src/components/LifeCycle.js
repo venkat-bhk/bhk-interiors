2545012748f13aa6b822f97db3dad5d438cf51df
Sai Krishna <krischadalawada@gmail.com>
2024-11-16 15:38:38 +0530

    Minor adjustments

--- src/components/LifeCycle.js
+++ src/components/LifeCycle.js
@@ -83,30 +83,32 @@ const LifeCycle = () => {
     <>
       <section className={`caret-hide`} id="plan">
         <div className="container lc-div">
-          <div className="lc-component" ref={elementRef}>
-            {
-              LCData.map((x, index) => (
-                <div className={`lc-element pointer ${inx === index && "lc-circle"}`} key={index + 1} onClick={() => onIconClick(index)}>
-                  <img
-                    src={x.img}
-                    className={`${inx !== index ? "lc-icon" : "lc-icon-sel"}`}
-                  />
-                  <h3 className={`fl fc-g ${inx === index ? "lc-title-sel" : "lc-title"} text-center ${width > height ? "fs-w-20" : "fs-w-32"}`}>{x.title}</h3>
-                </div>
-              ))
-            }
-            <div className="">
-              <img
-                src="/bhk-home/lifecycle/Big_Middle.svg"
-                className="lc-center"
-              />
-              <p className={`center-text fl lh-2 fc-o ${width > height ? "fs-w-40" : "fs-w-60"}`}>Design to delivery,<br /> in just 45 days!</p>
+          <div className="lc-component">
+            <div ref={elementRef}>
+              {
+                LCData.map((x, index) => (
+                  <div className={`lc-element pointer ${inx === index && "lc-circle"}`} key={index + 1} onClick={() => onIconClick(index)}>
+                    <img
+                      src={x.img}
+                      className={`${inx !== index ? "lc-icon" : "lc-icon-sel"}`}
+                    />
+                    <h3 className={`fl fc-g ${inx === index ? "lc-title-sel" : "lc-title"} text-center ${width > height ? "fs-w-20" : "fs-w-32"}`}>{x.title}</h3>
+                  </div>
+                ))
+              }
+              <div className="">
+                <img
+                  src="/bhk-home/lifecycle/Big_Middle.svg"
+                  className="lc-center"
+                />
+                <p className={`center-text fl lh-2 fc-o ${width > height ? "fs-w-40" : "fs-w-60"}`}>Design to delivery,<br /> in just 45 days!</p>
+              </div>
             </div>
             <div className="bottom-div">
               <p className={`fl fc-g  text-center ${width > height ? "fs-w-20" : "fs-w-60"}`}>Your dream home is just a few steps away.<br />Our streamlined process ensures a smooth journey <br />from concept to completion.</p>
               <div className={`d-flex justify-content-center ${width > height ? "p-w1" : "p-h1"}`}>
                 <Link legacyBehavior href="">
-                  <a className={`btn-main-b fl ${width > height ? "p-w-1 fs-w-20" : "p-h-1 fs-w-40"}`}>Get&nbsp;a&nbsp;quote!</a>
+                  <a className={`btn-main-b fl ${width > height ? "p-w-1 fs-w-20" : "p-h-1 fs-w-60"}`}>Get&nbsp;a&nbsp;quote!</a>
                 </Link>
               </div>
             </div>
